09d1d12cde77c19558e867f3fd98519f1e020893
danamestrial <chayapolbun@gmail.com>
2023-10-26 15:50:53 +0700

    feat: added a feature where refresh token is automatically fetched

--- src/main.ts
+++ src/main.ts
@@ -18,8 +18,41 @@ import 'vue-loading-overlay/dist/css/index.css';
 import { registerPlugins } from '@/plugins'
 
 import axios from 'axios';
+import createAuthRefreshInterceptor from 'axios-auth-refresh';
+import router from './router';
 
 axios.defaults.baseURL = '/api';
+
+// Function that will be called to refresh authorization
+const refreshAuthLogic = async () => {
+    await axios.get('/refresh').then(() => {
+        console.log("Token refreshed")
+    });
+    return Promise.resolve();
+}
+
+// Add a response interceptor
+axios.interceptors.response.use(function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response;
+  }, async function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    if (error.response.config.url == "/refresh" && error.response.status == 401)
+    {
+    router.go(0)
+    }
+    return Promise.reject(error);
+  });
+
+
+// Instantiate the interceptor
+createAuthRefreshInterceptor(
+    axios,
+    refreshAuthLogic
+    );
+
 
 const app = createApp(App)
 app.use(LoadingPlugin);
