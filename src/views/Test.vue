d76516898662e89a62f47bd43168a7d9ed3f8207
danamestrial <chayapolbun@gmail.com>
2023-11-01 18:55:17 +0700

    feat: changed video player back but using different implementation

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,31 +1,35 @@
 <template>
-<!-- <video-player
-    src="http://localhost:8000/api/m3u8/static/a98c6b2e-f574-44f6-b492-7e77311ae757/master.m3u8"
-    poster="https://toktik-s3-videos.sgp1.cdn.digitaloceanspaces.com/640872bc-85c2-4839-b356-89785156cd10/thumbnail.png"
-    controls
-    :loop="true"
-    :volume="0.6"
-    :html5="{vhs: {withCredentials: true}}"
-/> -->
-<VideoPlayer
-      type="default"
-      @pause="processPause"
-      previewImageLink="/api/static/a98c6b2e-f574-44f6-b492-7e77311ae757/thumbnail.png"
-      link="/api/m3u8/static/a98c6b2e-f574-44f6-b492-7e77311ae757/master.m3u8"
-      :isMuted="false"
-      :isControls="true"
-  />
+  <div>
+    <video-player :options="videoOptions" />
+  </div>
 </template>
 
 <script>
 // Documentation here Yang:
-// https://github.com/LeonidShv/vue-hls-video-player/tree/main
-import { defineComponent } from 'vue'
-import { VideoPlayer } from 'vue-hls-video-player';
+// https://videojs.com/getting-started
+import VideoPlayer from '@/components/VideoPlayer.vue';
 
-export default defineComponent({
-    components: {
+export default {
+  name: 'VideoExample',
+  components: {
     VideoPlayer
-    },
-})
+  },
+  data() {
+    return {
+      videoOptions: {
+        autoplay: true,
+        controls: true,
+        preload: 'auto',
+        poster: '/api/static/a98c6b2e-f574-44f6-b492-7e77311ae757/thumbnail.png',
+        sources: [
+          {
+            src:
+              '/api/m3u8/static/a98c6b2e-f574-44f6-b492-7e77311ae757/master.m3u8',
+              type: 'application/vnd.apple.mpegurl'
+          }
+        ]
+      }
+    };
+  }
+};
 </script>
