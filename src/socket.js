3815cf393916330bf57b1bea62ff8edf5dfcbbe1
danamestrial <chayapolbun@gmail.com>
2023-11-17 10:36:01 +0700

    fix: send socketio with credentials

--- src/socket.js
+++ src/socket.js
@@ -10,7 +10,10 @@ export const state = reactive({
 // "undefined" means the URL will be computed from the `window.location` object
 const URL = process.env.NODE_ENV === "production" ? undefined : "ws://toktik-socket:3030";
 
-export const socket = io(URL, { secure: true });
+export const socket = io(URL, {
+  secure: true,
+  withCredentials: true,
+});
 
 export const setSocket = (socketname) => {
     console.log("Now listening on: ", socketname);
