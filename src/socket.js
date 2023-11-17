9c2c60bf95599ea50f10a8ba2710dbc3b1785b0d
danamestrial <chayapolbun@gmail.com>
2023-11-17 14:21:24 +0700

    test: testing socket

--- src/socket.js
+++ src/socket.js
@@ -8,12 +8,12 @@ export const state = reactive({
 });
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === "production" ? "ws://ws.localhost" : "ws://toktik-socket:3030";
+const URL = process.env.NODE_ENV === "production" ? undefined : "ws://toktik-socket:3030";
 
 export const socket = io(URL, {
   // secure: true,
   // withCredentials: true,
-  transports: [ "websocket" ],
+  // transports: [ "websocket" ],
 });
 
 export const setSocket = (socketname) => {
