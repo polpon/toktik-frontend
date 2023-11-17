4c77f0b98403244b06fd6c1ff2716714384b261c
danamestrial <chayapolbun@gmail.com>
2023-11-17 10:51:37 +0700

    fix: trying to connect socket

--- src/socket.js
+++ src/socket.js
@@ -8,7 +8,7 @@ export const state = reactive({
 });
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === "production" ? undefined : "ws://toktik-socket:3030";
+const URL = process.env.NODE_ENV === "production" ? "ws://ws.localhost" : "ws://toktik-socket:3030";
 
 export const socket = io(URL, {
   secure: true,
